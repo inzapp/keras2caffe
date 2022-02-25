d3aee9d00c72c87940e3665b6e4cc66abd9e5888
inzapp <inzapp@naver.com>
2022-02-25 05:00:02 +0000

    [convert.sh] convert.py -> main.py

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
-python3 convert.py -sf tf -iw model.h5 -df caffe -om model --inNodeName=x --dstNodeName=Identity
+python3 main.py -sf tf -iw model.h5 -df caffe -om model --inNodeName=x --dstNodeName=Identity
 rm -rf __pycache__
