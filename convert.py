16e85023ef7281738449e47d646fd0d8c0175acf
inzapp <inzapp@naver.com>
2022-02-25 02:14:55 +0000

    init

---
+++ convert.py
@@ -0,0 +1,5 @@
+from mmdnn.conversion._script.convert import _main
+
+
+if __name__ == '__main__':
+    _main()
---
+++ convert.sh
@@ -0,0 +1,2 @@
+python3 convert.py -sf tf -iw model.h5 -df caffe -om model --inNodeName=x --dstNodeName=Identity
+rm -rf __pycache__
