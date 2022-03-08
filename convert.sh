ff58308b87dde5145c31256fdf061c8be95c20cd
inzapp <inzapp@naver.com>
2022-03-08 05:34:13 +0000

    [convert.sh] remove dummy file after conversion

--- convert.sh
+++ convert.sh
@@ -1,2 +1,5 @@
 python3 main.py -sf tf -iw model.h5 -df caffe -om model --inNodeName=x --dstNodeName=Identity
+cp -af main.py main.py.bak
 rm -rf __pycache__
+rm *.pb *.npy *.json *.py
+mv main.py.bak main.py
